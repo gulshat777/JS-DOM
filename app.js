82aeaefcbe9b136b9e3d785dde363432d92b99e1
Gulsha Myratberdiyeva <gulshatm777@gmail.com>
2018-05-15 17:19:13 +0000

    it is version5

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-var titles = document.getElementsByClassName('title');
-
-//console.log(Array.isArray(titles));
-//console.log(Array.isArray(Array.from(titles)));
-
- Array.from(titles).forEach(function(item){
- console.log(item);
- })
- 
- const wmf = document.querySelector('#book-list li:nth-child(2) .name');
- // console.log(wmf);
- var books = document.querySelector('#book-list li .name');
- // console.log(books);
- books = document.querySelectorAll('books-list li .name');
- console.log(books);
-Array.from(books).forEach(function(book){
- console.log(book);
-})
-
 var books = document.querySelectorAll('#book-list li.name');
 Array.from(books).forEach(function(book){
 book.textContent+='(book title)';
@@ -24,14 +5,6 @@ book.textContent+='(book title)';
 const bookList = document.querySelector('#book-list');
 //bookList.inerHTML+='<p>This is how you add HTML</p>';
 
-
-const banner = document.querySelector('#page-banner');
-
-console.log('#page-banner node type is:' banner.nodeType);
-console.log('#page-banner node name is:' banner.nodeName);
-console.log('#page-banner has child nodes:' banner.hasChildNodes());
-const clonedBanner = banner.cloneNode(true);
-console.log(clonedBanner);
 
 
 
