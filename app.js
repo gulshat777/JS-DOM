d609a288f167eeea6101fef104cad81d1cab90f0
Gulsha Myratberdiyeva <gulshatm777@gmail.com>
2018-05-15 17:28:39 +0000

    YOUR MESSAGE

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-var books = document.querySelectorAll('#book-list li.name');
-Array.from(books).forEach(function(book){
-book.textContent+='(book title)';
-});
-const bookList = document.querySelector('#book-list');
-//bookList.inerHTML+='<p>This is how you add HTML</p>';
+const banner = document.querySelector('#page-banner');
+
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner has child nodes:', banner.hasChildNodes());
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
+
 
 
 
