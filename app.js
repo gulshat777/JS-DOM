6ae44d774a7c6b86cb73b9f41de33fc90311bc5b
Gulsha Myratberdiyeva <gulshatm777@gmail.com>
2018-05-17 18:12:58 +0000

    ten

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-var btns = document.querySelectorAll('book-list.delete');
-Array.from(btns).forEach(function(btn){
-    btn.addEventListener('click',function(e){
-        const li = e.targetElement;
-        li.parentNode.remoteChild(li)
-    })
-})
-const link = document.querySelector('#page-banner a');
-link.addEventListener('click', function(e){
-e.preventDeafult();
-console.log('navigation to',e.target.textContent,'was prevented');
+const list = document.querySelector('book-list ul');
+list.addEventListener('click', function(e){
+    if(e.target.className == 'delete'){
+        const li = e.target.parentElement;
+        list.removeChild(li);
+    }
 });
