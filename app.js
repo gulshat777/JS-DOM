eeaf072bd65b4914ddbb91164cf8663b214c1985
Gulsha Myratberdiyeva <gulshatm777@gmail.com>
2018-05-17 17:48:49 +0000

    nine

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const bookList = document.querySelector('#book-list');
-
-console.log('book-list next sibling is:',bookList.nextSubling);
-console.log('book-list next element sibling is:',bookList.nextElementSibling);
-
-console.log('book-list previous sibling is:',bookList.previousSibling);
-console.log('book-list previous element sibling is;',bookList.previousElementSibling);
-bookList.previousElement.querySelector('p').innerHTML += '<br/>Too cool for everyone else!'
+var btns = document.querySelectorAll('book-list.delete');
+Array.from(btns).forEach(function(btn){
+    btn.addEventListener('click,function(e){
+        const li = e.targetElement;
+        li.parentNode.remoteChild(li)
+    })
+})
+const link = document.querySelector('#page-banner a');
+link.addEventListener('click', function(e){
+e.preventDeafult();
+console.log('navigation to',e.target.textContent,'was prevented');
+});
